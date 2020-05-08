33b31b783d3df833b78b841fd4baa8e87ba0a7f8
Dominik Paszelke <paszelkedominik@gmail.com>
2020-05-08 16:57:14 +0200

    article about data types and immutability

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ const age = 30;
 
 console.log(`Hello my name is ${name}, i'm ${age} year old`);
 console.log(name + ' wita na swojej stronie');
+
+typeof name;
+ console.log(typeof name);
+ console.log(typeof age);
+ console.log(typeof computer);
  
 
