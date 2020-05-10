edbe13d54c38908fb185936b5461725b87a98fbf
Dominik Paszelke <paszelkedominik@gmail.com>
2020-05-10 19:59:40 +0200

    changed script src and make some modifications in js

---
+++ drugi.js
@@ -0,0 +1,4 @@
+/*wrap-up__js__week-two--js*/
+const text = document.querySelector(`.wrap-up__js__week-two--js`);
+text.innerHTML = `Ten tekst został stworzony przy pomocy javascript`;
+console.log(text.innerHTML);
