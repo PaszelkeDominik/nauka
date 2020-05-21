a8ddc4dbd7e760f5006f390bd3921e82f495a954
Dominik Paszelke <paszelkedominik@gmail.com>
2020-05-21 13:27:29 +0200

    new week + hamburger menu

---
+++ hamburger-menu.js
@@ -0,0 +1,9 @@
+const navigaton = document.querySelector(`.navigaton--js`);
+const menu =document.querySelector(`.navigation__menu--js`);
+
+menu.addEventListener(`click`,()=>{
+  navigaton.classList.toggle(`navigation--show`);
+})
+navigaton.addEventListener(`mouseleave`, ()=>{
+  navigaton.classList.remove(`navigation--show`);
+})
