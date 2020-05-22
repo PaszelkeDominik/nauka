92f38f866790ec1347e375f3a3087ed8cb1287ff
Dominik Paszelke <paszelkedominik@gmail.com>
2020-05-22 16:29:21 +0200

    JS events

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -1,6 +1,7 @@
 const navigaton = document.querySelector(`.navigaton--js`);
 const menu = document.querySelector(`.navigation__menu--js`);
 
+
 menu.addEventListener(`click`, () => {
   navigaton.classList.toggle(`navigation--show`);
 });
