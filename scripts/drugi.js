f0573e2837dab8562ae17ba853353edc2e018a6b
Dominik Paszelke <paszelkedominik@gmail.com>
2020-05-26 14:29:08 +0200

    i put scripts files to separate directory,and fix src path in html + add to navigation new subsite link

---
+++ scripts/drugi.js
@@ -0,0 +1,8 @@
+/*wrap-up__js__week-two--js*/
+
+const text = document.querySelector(`.wrap-up__js__week-two--js`);
+
+text.innerHTML = `<p style="color:red; font-size:larger; font-family:cursive;">Ten tekst został stworzony przy pomocy javascript. Mówiłem, że czary!!</p>`;
+console.log(text.innerHTML);
+
+
